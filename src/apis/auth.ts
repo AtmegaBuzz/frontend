92537881381c024da4ee6443aec976c81bb197f0
Swapnil Shinde <swapnilshinde9382@gmail.com>
2025-05-20 17:37:51 +0530

    api imeplementation for create token and auth

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,31 +1,35 @@
 import axios from "axios";
 
+export async function login(authToken: string) {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/auth/login`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    );
 
-export async function login(wallet_address: string, public_key: string, message: string, signature: string) {
-    try {
-
-        const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
-            wallet_address,
-            public_key,
-            message,
-            signature
-        });
-
-        console.log("Access Token:", response.data.access_token);
-        return {
-            message: response.data.access_token,
-            errorType: null,
-            statusCode: 200
-        }
-    } catch (error) {
-
-        if (axios.isAxiosError(error) && error.response) {
-            const { message, error: errType, statusCode } = error.response.data;
-            console.error(`Error ${statusCode}: ${message} (${errType})`);
-            return { message, error: errType, statusCode }
-        } else {
-            console.error("Unexpected error:", error);
-        }
-        return { message: 'Something went wrong!', error: "danger", statusCode: 404 };
+    console.log("Access Token:", response.data.access_token);
+    return {
+      message: response.data.access_token,
+      errorType: null,
+      statusCode: 200,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const { message, error: errType, statusCode } = error.response.data;
+      console.error(`Error ${statusCode}: ${message} (${errType})`);
+      return { message, error: errType, statusCode };
+    } else {
+      console.error("Unexpected error:", error);
     }
+    return {
+      message: "Something went wrong!",
+      error: "danger",
+      statusCode: 404,
+    };
+  }
 }
